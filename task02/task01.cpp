c973e64fb69dd3dcfe7e090315b57ff52d198f03
Tim Zolleis <tim@zolleis.net>
2024-10-22 16:09:33 +0200

    fix(task02): fix test cases

--- task02/task01.cpp
+++ task02/task01.cpp
@@ -1,26 +1,28 @@
 #include <cassert>
 
-bool toInt(const char character, int *theInt) {
-    if (character >= '0' && character <= '9') {
-        //Subtracting the ASCII value of '0' from the character gives us the integer value of the character
-        *theInt = character - '0';
-        return true;
+namespace tz {
+    bool toInt(const char character, int &theInt) {
+        if (character >= '0' && character <= '9') {
+            //Subtracting the ASCII value of '0' from the character gives us the integer value of the character
+            theInt = character - '0';
+            return true;
+        }
+        return false;
     }
-    return false;
-}
 
-void toIntTestAccordingToTask2() {
-    int number = -1;
-    toInt('0', &number);
-    assert(number == 0);
-    number = -1;
-    toInt('9', &number);
-    assert(number == 9);
-    number = -1;
-    toInt('x', &number);
-    assert(number == -1);
+    void toIntTestAccordingToTask2() {
+        int number = -1;
+        toInt('0', number);
+        assert(number == 0);
+        number = -1;
+        toInt('9', number);
+        assert(number == 9);
+        number = -1;
+        toInt('x', number);
+        assert(number == -1);
+    }
 }
 
 int main() {
-    toIntTestAccordingToTask2();
+    tz::toIntTestAccordingToTask2();
 }
--- task02/task02.cpp
+++ task02/task02.cpp
@@ -2,46 +2,48 @@
 #include <stdexcept>
 #include <string>
 
-bool toInt(const char character, int *theInt) {
-    if (character >= '0' && character <= '9') {
-        //Subtracting the ASCII value of '0' from the character gives us the integer value of the character
-        *theInt = character - '0';
-        return true;
-    }
-    return false;
-}
-
-int decode(const char *line) {
-    int firstDigit = -1, lastDigit = -1, currentDigit;
-    for (int iterator = 0; line[iterator] != '\0'; ++iterator) {
-        if (toInt(line[iterator], &currentDigit)) {
-            if (firstDigit == -1) firstDigit = currentDigit;
-            lastDigit = currentDigit;
+namespace tz {
+    bool toInt(const char character, int *theInt) {
+        if (character >= '0' && character <= '9') {
+            //Subtracting the ASCII value of '0' from the character gives us the integer value of the character
+            *theInt = character - '0';
+            return true;
         }
+        return false;
     }
-    if (firstDigit == -1) throw std::invalid_argument("no digits found");
-    return (firstDigit * 10) + lastDigit;
-}
 
-void decodeTest() {
-    const std::string lines[] = {
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet"
-    };
-    constexpr int expected[]{12, 38, 15, 77};
-    for (int i = 0; i < 4; ++i) {
-        assert(decode(lines[i].c_str()) == expected[i]);
+    int decode(const std::string &line) {
+        int firstDigit = -1, lastDigit = -1, currentDigit;
+        for (int iterator = 0; line[iterator] != '\0'; ++iterator) {
+            if (toInt(line[iterator], &currentDigit)) {
+                if (firstDigit == -1) firstDigit = currentDigit;
+                lastDigit = currentDigit;
+            }
+        }
+        if (firstDigit == -1) throw std::invalid_argument("no digits found");
+        return (firstDigit * 10) + lastDigit;
     }
-    try {
-        decode("no digit");
-        assert(false);
-    } catch (std::invalid_argument &e) {
-        assert(true);
+
+    void decodeTest() {
+        const std::string lines[] = {
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
+        };
+        constexpr int expected[]{12, 38, 15, 77};
+        for (int i = 0; i < 4; ++i) {
+            assert(decode(lines[i]) == expected[i]);
+        }
+        try {
+            decode("no digit");
+            assert(false);
+        } catch (std::invalid_argument &e) {
+            assert(true);
+        }
     }
 }
 
 int main() {
-    decodeTest();
+    tz::decodeTest();
 }
